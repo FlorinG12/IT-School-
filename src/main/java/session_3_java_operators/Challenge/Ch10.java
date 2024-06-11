d465abf060a7bcd456489bb8fd8d5f7f8bda844e
Florin <f_ionut47@yahoo.com>
2024-06-11 21:24:39 +0300

    Session 3 - Java operators

---
+++ src/main/java/session_3_java_operators/Challenge/Ch10.java
@@ -0,0 +1,20 @@
+package session_3_java_operators.Challenge;
+
+import java.util.Scanner;
+
+public class Ch10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your integer : ");
+        int number = scanner.nextInt();
+
+        int reverse = -number;
+        System.out.println("Your reverse number is : " + reverse);
+
+//        int x = 12;
+//        int y =- x;
+//
+//        System.out.println(y);
+    }
+}
---
+++ src/main/java/session_3_java_operators/Challenge/Ch11.java
@@ -0,0 +1,23 @@
+package session_3_java_operators.Challenge;
+
+import java.util.Scanner;
+
+public class Ch11 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("The answer is switch so be carefull :) ");
+        System.out.println("Is java fun ? true/false : ");
+        boolean  isJavaFun = scanner.nextBoolean();
+        System.out.println("Do you like coding ? true/false");
+        boolean likeCoding = scanner.nextBoolean();
+
+    boolean first =!isJavaFun;
+    boolean second =!likeCoding;
+
+        System.out.println(first);
+        System.out.println(second);
+
+
+    }
+}
